4296339d92e920d93085ce407888efd52ca382d4
Sibaram <sibaramsamal04@gmail.com>
2022-04-15 22:35:39 +0530

    JDBC enhancements

--- JDBC Concepts/src/com/test/jdbc/UseOfPreparedStatement.java
+++ JDBC Concepts/src/com/test/jdbc/UseOfPreparedStatement.java
@@ -5,8 +5,8 @@ import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.util.Scanner;
 
-class PreparedStatementDemo{
-	public static void insertReccord(int sid,String sname,String gender,String address) {
+class PreparedStatementDemo {
+	public static void insertReccord(int sid, String sname, String gender, String address) {
 		String QUERY = "insert into Student_details values(?,?,?,?)";
 		int result = 0;
 		Connection con = ConnectionProvider.getConnection();
@@ -16,13 +16,13 @@ class PreparedStatementDemo{
 			ps.setString(2, sname);
 			ps.setString(3, gender);
 			ps.setString(4, address);
-			
+
 			result = ps.executeUpdate();
-			
+
 		} catch (SQLException e) {
 			e.printStackTrace();
 		}
-		if(result == 0)
+		if (result == 0)
 			System.out.println("failed to store Details");
 		else
 			System.out.println("Details stored successfully");
@@ -31,18 +31,18 @@ class PreparedStatementDemo{
 
 public class UseOfPreparedStatement {
 	public static void main(String[] args) {
-	Scanner sc = new Scanner(System.in);
-	System.out.println("======  Dynamic Record Insertion  ======");
-	System.out.println("Enter id: ");
-	int id = sc.nextInt();
-	System.out.println("Enter Name: ");
-	String name = sc.next();
-	name += sc.next();
-	System.out.println("Enter gender: ");
-	String gender = sc.next();
-	System.out.println("Enter address: ");
-	String address = sc.next();
-	
-	PreparedStatementDemo.insertReccord(id, name, gender, address);
+		Scanner sc = new Scanner(System.in);
+		System.out.println("======  Dynamic Record Insertion  ======");
+		System.out.println("Enter id: ");
+		int id = sc.nextInt();
+		System.out.println("Enter Name: ");
+		String name = sc.next();
+		name += sc.next();
+		System.out.println("Enter gender: ");
+		String gender = sc.next();
+		System.out.println("Enter address: ");
+		String address = sc.next();
+		sc.close();
+		PreparedStatementDemo.insertReccord(id, name, gender, address);
 	}
 }
