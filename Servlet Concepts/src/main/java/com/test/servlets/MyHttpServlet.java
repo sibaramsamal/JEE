8a87230394e3736e3a13636ca3af029db8f20177
Sibaram <sibaramsamal04@gmail.com>
2022-04-17 19:27:05 +0530

    User servlet using HttpServlet

---
+++ Servlet Concepts/src/main/java/com/test/servlets/MyHttpServlet.java
@@ -0,0 +1,26 @@
+package com.test.servlets;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+
+@WebServlet("/MyHttpServlet")
+public class MyHttpServlet extends HttpServlet {
+	
+	  public void service(ServletRequest req,ServletResponse res) {
+	  System.out.println("Hello"); }
+	 
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.getWriter().append("Served at: ").append(request.getContextPath());
+	}
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doGet(request, response);
+	}
+}
