16729623c1ef94d7e77e2d52404765c15f813019
Sibaram <sibaramsamal04@gmail.com>
2022-04-18 18:03:35 +0530

    Styling Servlet displaying view(o/p view)

--- Servlet Concepts/src/main/java/com/implementation/DisplayFormData.java
+++ Servlet Concepts/src/main/java/com/implementation/DisplayFormData.java
@@ -17,12 +17,20 @@ public class DisplayFormData extends HttpServlet {
 		String gender = request.getParameter("gen");
 		String age = request.getParameter("age");
 		
-		response.getWriter().println("Name: "+name);
-		response.getWriter().println("Gender: "+gender);
-		response.getWriter().println("Age: "+age);
+		response.getWriter().print(
+									"<html>"+
+											"<body style='text-align:center'>"+
+												"<h1>Displaying User data</h1>"+
+												"<h4 style='display:inline'> Name: </h4>"+name+
+												"<h4 style='display:inline'> Gender: </h4>"+gender+
+												"<h4 style='display:inline'> Age: </h4>"+age+
+											"</body>"+
+									"</html>"	
+									);
+		//response.getWriter().println("Gender: "+gender);
+		//response.getWriter().println("Age: "+age);
 	 }
 	public void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		doGet(request, response);
 	}
-
 }
